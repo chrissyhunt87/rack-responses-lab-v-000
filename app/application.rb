7424f7c5e979ca243bc68e773dac3763c661b847
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-12-07 00:21:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     timestamp = Time.now.to_i
-    if timestamp <= 12
-      resp.write "Good Morning!"
-    else
+    if timestamp > 12
       resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
     end
     resp.finish
   end
